17d1427b26dbafa687a8d2ef5f5df61a11d3b26e
Cory Knapp <knapp.cory@gmail.com>
2015-08-09 21:43:35 -0700

    added some internal const correctness

--- pathfinding.hpp
+++ pathfinding.hpp
@@ -49,21 +49,12 @@ public:
 		m_adaptor = std::unique_ptr<ADAPTOR>( new ADAPTOR( args... ) );
 		//create the first internal node, the ancestor to all nodes, and
 		//initialize it.
-		InternalNode * startNodePtr = newInternalNode( nullptr, &start );
-
+		const InternalNode * startNodePtr = newInternalNode( nullptr, &start );
 		m_openList.push_back( startNodePtr );
+		const InternalNode * q = popInternalNode();
 
 		while( !m_openList.empty() ){
 			// find the node on the open list with the lowest 'f' score.
-			std::sort( m_openList.begin(), m_openList.end(),
-				//TODO we don't really need to sort this list, we just need to
-				//find the smallest f value.
-				[]( const InternalNode* a, const InternalNode* b ) -> bool{
-					return a->f > b->f;
-				});
-    		//pop q off the open list
-			InternalNode * q = m_openList[ m_openList.size()-1 ];
-			m_openList.pop_back();
 			//generate q's successors
 			auto successorList =
 				m_adaptor->getAdjacentNodes(*(q->externalNode));
@@ -77,7 +68,7 @@ public:
 				}
 				// create and initialize a new internal node for each of q's
 				// successors
-				InternalNode * newNodePtr = newInternalNode( q, externalNodeSuccessor);
+				const InternalNode * newNodePtr = newInternalNode( q, externalNodeSuccessor);
 				
 				//addFlag can be changed by the two conditionals below if the
 				//new node is unsuitable.
@@ -86,7 +77,7 @@ public:
 				//list which has a lower f than successor, skip this successor
 				auto internalWithSameExternal = std::find_if(
 					m_openList.begin(), m_openList.end(),
-					[&newNodePtr](InternalNode * &oldNodePtr){
+					[&newNodePtr](const InternalNode * &oldNodePtr){
 						return *(newNodePtr->externalNode) ==
 							*(oldNodePtr->externalNode);
 					});
@@ -98,7 +89,7 @@ public:
 				// list which has a lower f than successor, skip this successor
         		internalWithSameExternal = std::find_if(
 					m_closedList.begin(), m_closedList.end(),
-					[&newNodePtr](InternalNode * &oldNodePtr){
+					[&newNodePtr](const InternalNode * &oldNodePtr){
 						return *(newNodePtr->externalNode) ==
 							*(oldNodePtr->externalNode);
 					});
@@ -120,7 +111,7 @@ public:
 		// m_lastNode is still set to nullptr, and return an empty path.
 		clean();
 	}
-
+	
 	std::vector<const node_t*> path(){
 		// the path is not yet in a usable form.  We need to copy it into a
 		// nice vector, without all of our internal node garbage.
@@ -130,7 +121,7 @@ public:
 			//back to front as we travel our graph from end to start
 			std::vector<const node_t*> returnList;
 			returnList.resize( m_lastNode->graphLength );
-			InternalNode * runner = m_lastNode;
+			const InternalNode * runner = m_lastNode;
 			size_t index = m_lastNode->graphLength-1;
 			while( runner ){
 				returnList[index] = runner->externalNode;
@@ -149,10 +140,18 @@ private:
 	//track of some stuff for our algorithm to work.
 	struct InternalNode{
 		
+		InternalNode( const node_t * externalNode,
+					  const InternalNode * parent,
+					  unsigned f, unsigned g, unsigned h,
+					  unsigned graphLength ) :
+    		externalNode( externalNode ),
+    		parent( parent ),
+    		f( f ), g( g ), h( h ),
+    		graphLength( graphLength ){}
 		const node_t * externalNode;
-		InternalNode * parent;
-		unsigned f, g, h;
-		unsigned graphLength;
+		const InternalNode * parent;
+		const unsigned f, g, h;
+		const unsigned graphLength;
 		
 		~InternalNode(){
 			std::cout << "deleting " << externalNode << std::endl;  
@@ -163,41 +162,47 @@ private:
 	//wrapped in a unique_ptr, so when we call clean, or the Search goes out of
 	//scope, we can be sure we didn't leak any nodes out of what can end up
 	//being a complicated set of graphs.  Poor man's garbage collector.
-	InternalNode * newInternalNode( InternalNode * parent,
+	const InternalNode * newInternalNode( const InternalNode * parent,
 								    const node_t * externalNode){
 
-		m_nodeList.push_back( std::unique_ptr<InternalNode>( new InternalNode ) );
-		auto newNode = m_nodeList[ m_nodeList.size() - 1 ].get();
-		newNode->externalNode = externalNode;
-		newNode->parent = parent;
+		InternalNode * newNode;
 		
 		std::cout << "new internal = " << externalNode << std::endl
 		;
 		
 		if( parent == nullptr ){
-			newNode->f = 0;
-			newNode->g = 0;
-			newNode->h = 0;
-			newNode->graphLength = 1
-			;
+			newNode = new InternalNode( externalNode, parent, 0, 0, 0, 1);
 		} else {
 			//calculate the new node's scored
-			newNode->g = parent->g +
-				m_adaptor->heuristicDistanceBetweenAdjacentNodes(
-					*newNode->externalNode, *parent->externalNode );
-			newNode->h =
-				m_adaptor->heuristicDistanceBetweenAdjacentNodes(
-					*newNode->externalNode, end );
-			newNode->f = newNode->g + newNode->h;
-			newNode->graphLength = parent->graphLength+1;
+			newNode = new InternalNode( externalNode, parent,
+					newNode->g + newNode->h,
+					parent->g + m_adaptor->heuristicDistanceBetweenAdjacentNodes(
+						*externalNode, *parent->externalNode ),
+    				m_adaptor->heuristicDistanceBetweenAdjacentNodes(
+					*externalNode, end ),
+					parent->graphLength+1 );
 
 		}
+		m_nodeList.push_back( std::unique_ptr<InternalNode>( newNode ) );
 		return newNode;
 	}
 	void clean(){
 		m_nodeList.clear();
 	}
 
+	const InternalNode * popInternalNode(){
+		std::sort( m_openList.begin(), m_openList.end(),
+			//TODO we don't really need to sort this list, we just need to
+			//find the smallest f value.
+			[]( const InternalNode* a, const InternalNode* b ) -> bool{
+				return a->f > b->f;
+			});
+		//pop q off the open list
+		const InternalNode * q = m_openList[ m_openList.size()-1 ];
+		m_openList.pop_back();
+		return q;
+	}
+	
 	// we need to know the end node when we calculate scores in
 	// newInternalNode()
 	const node_t &end;
@@ -205,13 +210,13 @@ private:
 	//we have to use a list of pointers, rather then contiguous memory because
 	//of the graph structure.  We can't have InternalNodes moving around in
 	//memory.
-	std::vector<InternalNode*> m_openList;
-	std::vector<InternalNode*> m_closedList;
+	std::vector<const InternalNode*> m_openList;
+	std::vector<const InternalNode*> m_closedList;
 
 	//for the poor man's garbage collector. 
 	std::vector<std::unique_ptr<InternalNode>> m_nodeList;
 
-	InternalNode * m_lastNode = nullptr;
+	const InternalNode * m_lastNode = nullptr;
 	
 	std::unique_ptr<ADAPTOR> m_adaptor;
 };
