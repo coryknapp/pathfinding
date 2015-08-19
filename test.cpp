d635e017c67915656856c1bb56715b1d0efe597f
Cory Knapp <knapp.cory@gmail.com>
2015-08-19 14:25:44 -0700

    added tests

--- test.cpp
+++ test.cpp
@@ -13,15 +13,7 @@ using namespace pf;
 
 TEST_CASE( "SimpleGraph", "[graph]" ) {
 
-	//There are two requirements for a 'node' class
-	//1.Your adaptor class must be able to find a node's adjacent nodes
-	//	using only it's members.  For example, SimpleNode below stores pointers
-	//	to it's adjacent nodes in a vector.
-	//2.The class must implement the '==' operator.  I've done so below by
-	//	comparing the address of both objects, which should be fine if the nodes
-	//	can be expected to be unique and never moved.  You can define your
-	//	node's equality however you want: comparing positions, some kind of id
-	//	code, etc. 
+	//the only requirement for a node is that it must implement '=='.
 	struct SimpleNode : boost::noncopyable{
 		
 		SimpleNode( char name ) : name(name) {}
@@ -34,7 +26,6 @@ TEST_CASE( "SimpleGraph", "[graph]" ) {
 		bool operator==(const SimpleNode& rhs) const{
 			return ( this == &rhs );
 		}
-		
 	};
 
 	class SimpleAdaptor : Adaptor<SimpleNode>
@@ -139,6 +130,126 @@ TEST_CASE( "Generating node's on the fly", "[graph]" ) {
 		}
 
 		std::vector<node_t*> getAdjacentNodes(const node_t& node){
+			//std::cout << "getAdjacentNodes("<<node.first<<','<<node.second<<")\n";
+			std::vector<std::pair<int,int>*> returnVector;
+			int i = node.first;
+			int j = node.second;
+			if( validNode( i-1, j ) )
+				returnVector.push_back( makeNode( i-1, j ) );
+			if( validNode( i+1, j ) )
+				returnVector.push_back( makeNode( i+1, j ) );
+			if( validNode( i, j-1 ) )
+				returnVector.push_back( makeNode( i, j-1 ) );
+			if( validNode( i, j+1 ) )
+				returnVector.push_back( makeNode( i, j+1 ) );
+			return returnVector;
+		}
+
+		unsigned heuristicDistanceBetweenAdjacentNodes(const node_t& a,
+													   const node_t& b){
+			return abs(a.first - b.first + a.second - b.second);
+		}
+	};
+
+	SECTION( "open grid" ) {
+		const bool grid[25] =
+				{	0,	0,	0,	0,	0,
+					0,	0,	0,	0,	0,
+					0,	0,	0,	0,	0,
+					0,	0,	0,	0,	0,
+					0,	0,	0,	0,	0 };
+		Search<MyAdaptor, const bool *> search(
+			std::pair<int,int>(0,0), std::pair<int,int>(4,4), grid );
+		auto path = search.path();
+		REQUIRE( path.size() == 9 );
+	}
+	
+	SECTION( "one path grid" ) {
+		const bool grid[25] =
+				{	0,	1,	0,	0,	0,
+					0,	1,	0,	0,	0,
+					0,	1,	0,	0,	0,
+					0,	1,	1,	1,	1,
+					0,	0,	0,	0,	0 };
+		Search<MyAdaptor, const bool *> search(
+			std::pair<int,int>(0,0), std::pair<int,int>(4,4), grid );
+		auto path = search.path();
+		REQUIRE( path.size() == 9 );
+		REQUIRE( path[4]->first ==0 );
+		REQUIRE( path[4]->second==4 );
+	}
+
+	SECTION( "better lower path grid" ) {
+		const bool grid[25] =
+				{	0,	0,	0,	0,	0,
+					0,	1,	1,	1,	0,
+					0,	1,	0,	0,	0,
+					0,	1,	0,	1,	1,
+					0,	0,	0,	0,	0 };
+		Search<MyAdaptor, const bool *> search(
+			std::pair<int,int>(0,0), std::pair<int,int>(4,4), grid );
+		auto path = search.path();
+		REQUIRE( path.size() == 9 );
+		REQUIRE( path[4]->first ==0 );
+		REQUIRE( path[4]->second==4 );
+	}
+
+		SECTION( "better upper path grid" ) {
+		const bool grid[25] =
+				{	0,	0,	0,	0,	0,
+					0,	1,	1,	1,	0,
+					0,	1,	0,	0,	0,
+					0,	1,	0,	1,	0,
+					0,	0,	0,	1,	0 };
+		Search<MyAdaptor, const bool *> search(
+			std::pair<int,int>(0,0), std::pair<int,int>(4,4), grid );
+		auto path = search.path();
+		REQUIRE( path.size() == 9 );
+		REQUIRE( path[4]->first ==4 );
+		REQUIRE( path[4]->second==0 );
+	}
+
+
+
+}
+
+TEST_CASE( "spied on optimization check", "[graph]" ) {
+
+	using namespace std;
+
+	//std::pair is a perfectly fine node
+	//it defines '=='.
+	typedef std::pair<int,int> MyNode;
+
+	class MyAdaptor : Adaptor<MyNode>
+	{
+	public:
+		using Adaptor::node_t;
+	 	const bool * grid;
+		
+		vector<unique_ptr<node_t> >garbageCollecter;
+		MyAdaptor( const bool * grid ) : grid(grid){
+		}
+		
+		bool validNode( int i, int j ){
+			if( ( i >= 5 )||( i < 0 ) )
+				return false;
+			if( ( j >= 5 )||( j < 0 ) )
+				return false;
+			return grid[i+j*5] == 0;
+		}
+			
+		node_t * makeNode( int i, int j ){
+			return new pair<int, int>( i, j);
+			garbageCollecter.push_back(
+				unique_ptr<std::pair<int, int>>(
+					new pair<int, int>(i,j)
+					)
+				);
+			return garbageCollecter[ garbageCollecter.size() - 1].get();
+		}
+
+		std::vector<node_t*> getAdjacentNodes(const node_t& node){
 			std::cout << "getAdjacentNodes("<<node.first<<','<<node.second<<")\n";
 			std::vector<std::pair<int,int>*> returnVector;
 			int i = node.first;
@@ -172,5 +283,4 @@ TEST_CASE( "Generating node's on the fly", "[graph]" ) {
 		auto path = search.path();
 		REQUIRE( path.size() == 9 );
 	}
-
 }
